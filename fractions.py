4f19c74057338183b6cc615b27907a73a3801143
vvkdhkl <dhakalvivek@gmail.com>
2019-09-16 21:47:59 +0300

    fixed whole numbers support

--- fractions.py
+++ fractions.py
@@ -7,21 +7,28 @@ def hcf(a, b):
 
 class fraction:
     def __str__(self):
-        return (str(self.numerator) + "/" + str(self.denominator))
+        if self.denominator == 1:
+            return str(self.numerator)
+        else:
+            return (str(self.numerator) + "/" + str(self.denominator))
 
-    def __init__(self, a, b):
-        if (a*b < 0):
-            a = -1*abs(a)
-            b = abs(b)
-        elif (a*b > 0):
-            a = abs(a)
-            b = abs(b)
-        elif (b==0):
-            return "Error: not a valid fraction."
+    def __init__(self, a, b = None):
+        if (b == None):
+            self.numerator = a
+            self.denominator = 1
+        else:
+            if (a*b < 0):
+                a = -1*abs(a)
+                b = abs(b)
+            elif (a*b > 0):
+                a = abs(a)
+                b = abs(b)
+            elif (b==0):
+                return "Error: not a valid fraction."
 
-        h = hcf(a, b)
-        self.numerator = int(a/h)
-        self.denominator = int(b/h)        
+            h = hcf(a, b)
+            self.numerator = int(a/h)
+            self.denominator = int(b/h)        
 
 def sum(fraction1, fraction2):
     return fraction(fraction1.numerator * fraction2.denominator + fraction1.denominator * fraction2.numerator, fraction1.denominator * fraction2.denominator)
@@ -43,8 +50,14 @@ b = fraction(4, 3)
 c = fraction(1, 2)
 d = fraction(-2, 5)
 e = fraction(6, 2)
+f = fraction(1)
+g = fraction(0)
+h = fraction(3)
 
 testSum(a, b, fraction(7, 12))
 testSum(b, d, fraction(14, 15))
 testSum(c, e, fraction(7, 2))
 testSum(a, d, fraction(-23, 20))
+testSum(f, g, f)
+testSum(g, g, g)
+testSum(h, f, fraction(4))
